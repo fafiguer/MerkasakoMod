747bd435825c690b0b43d79b422b7de119dc4403
Felipe Aguilera <pipealejandro.aguilera@gmail.com>
2020-12-09 02:13:25 -0300

    Se ha agregado el Merkasako en Tools
    Se ha agregado el Mekrasaco a la categoria Tools (Momentaneamente, luego se le hara una categoria exclusiva al mod para todos sus items) aun no tiene ni una sola funcionalidad solo esta el objeto en si.

---
+++ src/main/java/com/FafaTeam/MerkaSako/util/RegistryHandler.java
@@ -0,0 +1,20 @@
+package com.FafaTeam.MerkaSako.util;
+
+import com.FafaTeam.MerkaSako.MerkaSakoMod;
+import com.FafaTeam.MerkaSako.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MerkaSakoMod.MOD_ID);
+
+    public static void init(){
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    //Items
+    public static final RegistryObject<Item> MERKASAKO = ITEMS.register("merkasako", ItemBase::new);
+}
