88730f102294fc83ea93db295309cdca3fb6f4f5
Felipe Aguilera <pipealejandro.aguilera@gmail.com>
2020-12-10 19:23:02 -0300

    Pequeño fix
    Se ha fixeado un error, el cual hacia que no se viera correctamente el nombre de la tab exclusiva del mod por haber puesto mayuscula una letra que era minuscula.

--- src/main/java/com/FafaTeam/MerkaSako/util/RegistryHandler.java
+++ src/main/java/com/FafaTeam/MerkaSako/util/RegistryHandler.java
@@ -3,7 +3,7 @@ package com.FafaTeam.MerkaSako.util;
 import com.FafaTeam.MerkaSako.MerkaSakoMod;
 import com.FafaTeam.MerkaSako.items.ItemBase;
 import net.minecraft.item.Item;
-//import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,7 +16,7 @@ public class RegistryHandler {
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
-    //Items ("merkasako", () -> new Item(new Item.Properties().group(ItemGroup.instance).maxStackSize(1)));
+    //Items
     public static final RegistryObject<Item> MERKASAKO = ITEMS.register("merkasako", () -> new Item(new Item.Properties().group(MerkaSakoMod.TAB).maxStackSize(1)));
     public static final RegistryObject<Item> MERKAGEMA = ITEMS.register("merkagema", ItemBase::new);
     public static final RegistryObject<Item> FRAGMENTO_DE_MERKAGEMA = ITEMS.register("fragmento_de_merkagema", ItemBase::new);
