4adff32589200d7ff67fad078dcc1d4d3c67949c
angie <angheloalf95@gmail.com>
2020-12-15 15:44:46 -0300

    Agregar DeferredRegister de para el global_loot_modifier
    Signed-off-by: angie <angheloalf95@gmail.com>

--- src/main/java/com/FafaTeam/MerkaSako/MerkaSakoMod.java
+++ src/main/java/com/FafaTeam/MerkaSako/MerkaSakoMod.java
@@ -4,21 +4,25 @@ import com.FafaTeam.MerkaSako.util.RegistryHandler;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("merkasako")
-public class MerkaSakoMod
-{
-
+@Mod(MerkaSakoMod.MOD_ID)
+public class MerkaSakoMod{
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "merkasako";
+
+    public static final DeferredRegister<GlobalLootModifierSerializer<?>> GLOBAL_LOOT_MODIFIER = DeferredRegister.create(ForgeRegistries.LOOT_MODIFIER_SERIALIZERS, MerkaSakoMod.MOD_ID);
 
     public MerkaSakoMod() {
         // Register the setup method for modloading
@@ -27,6 +31,7 @@ public class MerkaSakoMod
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
         RegistryHandler.init();
+        GLOBAL_LOOT_MODIFIER.register(FMLJavaModLoadingContext.get().getModEventBus());
 
         MinecraftForge.EVENT_BUS.register(this);
     }
